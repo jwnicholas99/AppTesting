4fced2dc3dadc380d53f3d5c2db375daa3de2f91
Jing Wei Nicholas Lim <jingweinicholaslim@Jings-Mac.local>
2019-03-25 18:00:54 -0700

    first build

--- Simple Food Tracker/Simple Food Tracker/ViewController.swift
+++ Simple Food Tracker/Simple Food Tracker/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nameField: UILabel!
+    @IBOutlet weak var originField: UILabel!
+    @IBOutlet weak var priceField: UILabel!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var originText: UITextField!
+    @IBOutlet weak var priceText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    @IBAction func resetDefault(_ sender: UIButton) {
+        nameField.text = "Name?"
+        originField.text = "Origin?"
+        priceField.text = "Price?"
+    }
+    
+    @IBAction func updateButt(_ sender: UIButton) {
+        nameField.text = nameText.text
+        originField.text = originText.text
+        priceField.text = priceText.text
+    }
 }
 
